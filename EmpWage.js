1e9df40793deac94f55bb7e0dba1e4c67be6b2ef
Aanchalt14 <aanchalt71@gmail.com>
2020-12-16 01:30:05 +0530

    Added : Refactor the code

--- EmpWage.js
+++ EmpWage.js
@@ -1,10 +1,19 @@
-//UC-1 
-const IS_ABSENT =0
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if ( empCheck == IS_ABSENT) {
-    console.log("Employee is absent");
-    return ;
+//UC-3
+function getworkingHours(empCheck) {
+    switch  (empCheck) {
+        case IS_PART_TIME :
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
-else {
-     console.log("Employee is present");
-}
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() *10) %3;
+empHrs = getworkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("emp wage " +empWage);
+
+
