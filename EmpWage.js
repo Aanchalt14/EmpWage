c957dfab2e74e1fcf2725b2b13a2f86d4b491483
Aanchalt14 <aanchalt71@gmail.com>
2020-12-16 02:14:15 +0530

    Added : Calculate till condition satisfied

--- EmpWage.js
+++ EmpWage.js
@@ -1,19 +1,13 @@
-//UC-3
-function getworkingHours(empCheck) {
-    switch  (empCheck) {
-        case IS_PART_TIME :
-            return PART_TIME_HOURS;
-        case IS_FULL_TIME:
-            return FULL_TIME_HOURS;
-        default:
-            return 0;
+//UC-5 
+const MAX_HRS_IN_MONTH =100;
+const NUM_OF_WORKING_DAYS =10;
+let totalEmpHrs =0;
+let totalWorkingDays=0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
     }
-}
-
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() *10) %3;
-empHrs = getworkingHours(empCheck);
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("emp wage " +empWage);
-
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total days : " +totalWorkingDays + "total hrs : "+totalEmpHrs + "Emp wage :" +empWage);
 
