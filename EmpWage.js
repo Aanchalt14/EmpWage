83dfc4ed769095e5efde15e7bfb03a004aa6c64d
Aanchalt14 <aanchalt71@gmail.com>
2020-12-16 13:05:38 +0530

    Added : Attendence of employee

--- EmpWage.js
+++ EmpWage.js
@@ -1,13 +1,11 @@
-//UC-5 
-const MAX_HRS_IN_MONTH =100;
-const NUM_OF_WORKING_DAYS =10;
-let totalEmpHrs =0;
-let totalWorkingDays=0;
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-    totalWorkingDays++;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
-    }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("Total days : " +totalWorkingDays + "total hrs : "+totalEmpHrs + "Emp wage :" +empWage);
+//UC-1 
+const IS_ABSENT =0
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if ( empCheck == IS_ABSENT) {
+    console.log("Employee is absent");
+    return ;
+}
+else {
+     console.log("Employee is present");
+}
 
